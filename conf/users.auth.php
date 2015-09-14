217b15e9a28be2842113a3ffaf8bb4f84f081eac
RignonNoel <rignon.noel@openmailbox.org>
2015-09-14 16:43:38 -0400

    Add Dokuwiki

---
+++ conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+root:$1$Mb5FVBfK$cDY3n0Fq1dL/v7dG5JI8k0:RignonNoel:rignon.noel@openmailbox.org:admin,user
+rignonnoel:$1$gZNormWn$fKZ6SdS38mFbKAx.5.mMq/:Noël Rignon:rignon.noel@openmailbox.org:admin,user
